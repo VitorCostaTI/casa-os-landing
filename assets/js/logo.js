ab73dc9ea40766978a1bee527a777d83b182072c
Vitor Costa <vitortecnologia62@gmail.com>
2023-09-26 22:27:38 -0300

    Landing Page CasaOS

---
+++ assets/js/logo.js
@@ -0,0 +1,15 @@
+function logo(){
+    console.log("                                        ");
+    console.log("    ╔══╗                                ");
+    console.log("    ║    ╔═╗ ╔═╗ ╔═╗ ╔═╗ ╔═╗ ╔═╗ ╔═╗    ");
+    console.log("    ║    ║ ║ ╚═╗  ║  ╠═╣  ║  ║╣  ║      ");
+    console.log("    ╚══╝ ╚═╝ ╚═╝  ╩  ╩ ╩  ╩  ╚═╝ ╚═╝    ");
+    console.log(" ====================================== ");
+    console.log("                                        ");
+    console.log("       Versão: 1.0 - Comunidade         ");
+    console.log("                                        ");
+    console.log(" ====================================== ");
+    console.log("");
+};
+
+module.exports = logo();
